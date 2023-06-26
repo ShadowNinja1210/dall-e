9c66718c8dd368f8d07148b691c76ff82c1e20d0
Mohit Jeswani <mohitjeswani1210@gmail.com>
2023-06-26 23:51:26 +0530

    26 June, 2023 - Vercel config (MongoDB Atlas)

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
   const fetchPosts = async () => {
     setIsLoading(true);
     try {
-      const response = await fetch("http://localhost:8080/api/v1/posts", {
+      const response = await fetch("https://dall-e-two-iota.vercel.app/api/v1/posts", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.get("/", async (req, res) => {
 
 const startServer = async () => {
   try {
-    connectDB(process.env.MONGODB_URL);
+    connectDB(process.env.MONGODB_URI);
     app.listen(8080, () => console.log("Server is running on port 8080"));
   } catch (err) {
     console.log(err);
