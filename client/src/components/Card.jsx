bc1283729df88432d92720f8c259b2a4481b885e
Mohit Jeswani <mohitjeswani1210@gmail.com>
2023-06-27 17:32:04 +0530

    26 June, 2023 - Download button was not working (FIXED, checked on localhost)

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,9 +2,11 @@ import { download } from "../assets";
 import { downloadImage } from "../utils";
 
 const Card = ({ _id, name, prompt, photo }) => {
+  const securePhoto = photo.replace("http://", "https://");
+
   return (
     <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">
-      <img className="w-full h-auto object-cover rounded-xl" src={photo} alt={prompt} />
+      <img className="w-full h-auto object-cover rounded-xl" src={securePhoto} alt={prompt} />
       <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
         <p className="text-white text-md overflow-y-auto prompt">{prompt}</p>
 
@@ -14,7 +16,7 @@ const Card = ({ _id, name, prompt, photo }) => {
             <p className="text-white text-sm">{name}</p>
           </div>
 
-          <button type="button" onClick={() => downloadImage(_id, photo)} className="outline-none bg-transparent border-none">
+          <button type="button" onClick={() => downloadImage(_id, securePhoto)} className="outline-none bg-transparent border-none">
             <img src={download} alt="download" className="w-6 h-6 object-contain invert" />
           </button>
         </div>
